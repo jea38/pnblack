4128729bf734edd8c28b8fb62b094f9aabda9558
Jeff <jeff.attopany@gmail.com>
2024-10-09 18:04:57 -0400

    Delete pnblack directory

--- pnblack/js/p_view.js
+++
@@ -1,9 +0,0 @@
-const sliderMainImage = document.getElementById("product-main-image"); // Product container image
-const sliderImageList = document.getElementsByClassName("image-list"); // Image thumbnail group selection
-
-for (let i = 0; i < sliderImageList.length; i++) {
-  sliderImageList[i].onclick = function () {
-    sliderMainImage.src = sliderImageList[i].src;
-    console.log(sliderMainImage.src);
-  };
-}
--- pnblack/lvs/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-// Destroy session data
-session_destroy();
-// Redirect to login page
-header('Location: login.php');
-?>
