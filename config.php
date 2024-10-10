b3674c8a09ac135938cb720155820c0b24857c57
Jeff <jeff.attopany@gmail.com>
2024-10-09 20:07:23 -0400

    Update config.php

--- config.php
+++ config.php
@@ -42,11 +42,11 @@ define('paypal_testmode',true);
 // Currency to use with PayPal, default is USD
 define('paypal_currency','USD');
 // PayPal IPN url, this should point to the IPN file located in the "ipn" directory
-define('paypal_ipn_url','https://pnblack.com/ipn/paypal.php');
+define('paypal_ipn_url','https://pnblack.azurewebsites.net/ipn/paypal.php');
 // PayPal cancel URl, the page the customer returns to when they cancel the payment
-define('paypal_cancel_url','https://pnblack.com/cart');
+define('paypal_cancel_url','https://pnblack.azurewebsites.net/cart');
 // PayPal return URL, the page the customer returns to after the payment has been made:
-define('paypal_return_url','https://pnblack.com/placeorder');
+define('paypal_return_url','https://pnblack.azurewebsites.net/placeorder');
 
 /* Stripe */
 // Accept payments with Stripe?
@@ -58,11 +58,11 @@ define('stripe_enabled',true);
 // Stripe currency
 define('stripe_currency','USD');
 // Stripe IPN url, this should point to the IPN file located in the "ipn" directory
-define('stripe_ipn_url','https://pnblack.com/ipn/stripe.php');
+define('stripe_ipn_url','https://pnblack.azurewebsites.net/ipn/stripe.php');
 // Stripe cancel URl, the page the customer returns to when they cancel the payment
-define('stripe_cancel_url','https://pnblack.com/cart');
+define('stripe_cancel_url','https://pnblack.azurewebsites.net/cart');
 // Stripe return URL, the page the customer returns to after the payment has been made
-define('stripe_return_url','https://pnblack.com/placeorder');
+define('stripe_return_url','https://pnblack.azurewebsites.net/placeorder');
 // Stripe webhook secret, this is used to verify the webhook request
 //define('stripe_webhook_secret','whsec_5MD3OxwjGCrm0uACFOnDdSBi8TWLLvC5');
 
@@ -78,7 +78,7 @@ define('coinbase_secret','d1d443c2-008f-4f96-9be9-0cce22282315');
 // Coinbase currency
 define('coinbase_currency','USD');
 // Coinbase cancel URl, the page the customer returns to when they cancel the payment
-define('coinbase_cancel_url','https://pnblack.com/cart');
+define('coinbase_cancel_url','https://pnblack.azurewebsites.net/cart');
 // Coinbase return URL, the page the customer returns to after the payment has been made
-define('coinbase_return_url','https://pnblack.com/placeorder');
+define('coinbase_return_url','https://pnblack.azurewebsites.net/placeorder');
 ?>
